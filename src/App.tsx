cfc02006869bcb25e69f56aef1c42d3c4196e085
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-06-27 18:04:07 +0530

    Created Background..

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { ThreeDCardDemo } from "./components/3Dcard/3d-cardComponents";
+// import { ThreeDCardDemo } from "./components/3Dcard/3d-cardComponents";
+// import { SparklesPreview } from "./components/Sparkles/sparkleComponent";
+import { SparklesCore } from "./components/Sparkles/sparkles";
+import { SparklesPreview } from "./components/Sparkles/sparklesBackground";
 
 
 function App() {
 
   return (
     <>
-      <ThreeDCardDemo></ThreeDCardDemo>
+      {/* <ThreeDCardDemo></ThreeDCardDemo> */}
+      <SparklesPreview></SparklesPreview>
+      {/* <SparklesPreview></SparklesPreview> */}
     </>
   )
 }
--- src/components/3Dcard/3d-cardComponents.tsx
+++ src/components/3Dcard/3d-cardComponents.tsx
@@ -2,6 +2,7 @@
 
 import { CardBody, CardContainer, CardItem } from "./3d-card";
 
+
 export function ThreeDCardDemo() {
   return (
     <CardContainer className="inter-var">
